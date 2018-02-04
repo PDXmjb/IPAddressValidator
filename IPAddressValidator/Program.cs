b5d198e46313d6fd17e928e8376a032bcd445fca
Mike Brooks <PDXmjb@users.noreply.github.com>
2018-02-03 19:52:52 -0800

    Validates that a given IP address is valid.
    An algorithm that will identify valid IPv4 addresses in dot-decimal format. IPs should be considered valid if they consist of four octets, with values between 0..255 (included).
    
    Input to the function is guaranteed to be a single string.
    
    Examples
    // valid inputs:
    1.2.3.4
    123.45.67.89
    
    // invalid inputs:
    1.2.3
    1.2.3.4.5
    123.456.78.90
    123.045.067.089
    Note: leading zeros (e.g. 01.02.03.04) are considered not valid!

--- IPAddressValidator/Program.cs
+++ IPAddressValidator/Program.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IPAddressValidator
+﻿namespace Solution
 {
-    class Program
+    using System;
+    using System.Linq;
+    class Kata
     {
-        static void Main(string[] args)
+        public static bool is_valid_IP(string ipAddress)
         {
+            var octets = ipAddress.Split('.');
+
+            // If the given string is empty or of the wrong length, it is invalid.
+            var valid = ipAddress == String.Empty || octets.Length != 4 ? false : true;
+
+            foreach (var o in octets.Where(x => x != String.Empty))
+            {
+                int i = 0;
+                var isInt = Int32.TryParse(o, out i);
+                var hasLeadingZero = o[0] == '0' && i > 0;
+                var isOutsideOfBounds = i < 0 || i > 255;
+                var containsSpaces = o.Contains(' ');
+
+                // String has to a) be an integer; b) not have a leading zero if it's a number,
+                // c) be from 0-255; d) contain no spaces.
+                if (!isInt || hasLeadingZero || isOutsideOfBounds || containsSpaces)
+                {
+                    valid = false;
+                    break;
+                }
+            }
+            return valid;
         }
     }
 }
